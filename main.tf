f4b9c8cf7747ce2c598f2fea8c5990944732a393
Rafael Portillo <rpgd60@users.noreply.github.com>
2021-02-10 13:53:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ provider "aws" {
   region = "eu-west-3"
   profile = "radmin1"
 }
+
+# VPC Resource
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "test-vpc1"
+    disposable = "yes"
+  }
+}
+
+
